50320fe663bc5a1e54e89712c318437d381cfeec
Dexter <26281791+dextersjab@users.noreply.github.com>
2022-11-06 03:02:09 +0000

    test updates

--- jest.config.ts
+++ jest.config.ts
@@ -3,12 +3,6 @@ import type {Config} from 'jest';
 
 const config: Config = {
   verbose: true,
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  "moduleDirectories": [
-    "<rootDir>",
-    "node_modules"
-  ]
 };
 
 export default config;
--- test/util/mockUtils.ts
+++ test/util/mockUtils.ts
@@ -1,68 +1,45 @@
 import * as fs from "fs";
-// import config from "../../util/config/config";
-import {ClientMetadata, TokenSet} from "@dextersjab/openid-client"
+import {ClientMetadata} from "@dextersjab/openid-client"
 import axios from "axios";
 import clientData, {ClientRecord} from "../../src/data/clientData";
 
 const mockResponsesFilePath = "test/testdata/mockResponses.json"
 const mockResponses = JSON.parse(fs.readFileSync(mockResponsesFilePath).toString())
-
-// const regIDs = ['EXAMPLE_SOFTWARE_ID', 'EXAMPLE_SOFTWARE_ID_2']
 const providerIDs = ['XXX', 'YYY']
 
 export const mockDbResponses = (mockedClientData: jest.Mocked<typeof clientData>) => {
-  mockedClientData.createClient.mockImplementation((_, __) => {
+  mockedClientData.createClient = jest.fn((_, __, ___) => {
     return Promise.resolve(true)
   })
-  // mockedClientData.getProviderIDs.mockImplementation(() => {
-  //   return Promise.resolve(regIDs)
-  // })
+
+  mockedClientData.createClient = jest.fn((_, __, ___) => {
+    return Promise.resolve(true)
+  })
   const mockClientMetadata: ClientMetadata = JSON.parse(fs.readFileSync('test/testdata/testClient.json').toString()) as ClientMetadata
   const mockClientRecord: ClientRecord = {
     clientID: "",
     openIDConfigUrl: "https://auth.abcbank.com/.well-known/openid-configuration",
     metadata: mockClientMetadata,
   }
-  mockedClientData.getClient.mockImplementation((clientID) => {
+  mockedClientData.getClient = jest.fn((clientID) => {
     return Promise.resolve(providerIDs.includes(clientID) ? mockClientRecord : undefined)
   })
-  mockedClientData.clientExists.mockImplementation((provider) => {
+  mockedClientData.clientExists = jest.fn((provider) => {
     return Promise.resolve(provider === 'XXX')
   })
-  mockedClientData.getClientIDs.mockImplementation(() => {
+  mockedClientData.getClientIDs = jest.fn(() => {
     return Promise.resolve(['XXX', 'YYY'])
   })
 }
 
 export const mockAPIResponses = (mockedAxios: jest.Mocked<typeof axios>) => {
-  mockedAxios.get.mockImplementation((url, c) => {
+  mockedAxios.get = jest.fn((url, c) => {
     const mockResponse = mockResponses[url]
     return Promise.resolve(mockResponse ? mockResponse : {status: 404})
   })
-  mockedAxios.create.mockImplementation((config) => mockedAxios)
-  mockedAxios.post.mockImplementation((url, c) => {
+  mockedAxios.create = jest.fn((config) => mockedAxios)
+  mockedAxios.post = jest.fn((url, c) => {
     const mockResponse = mockResponses[url]
     return Promise.resolve(mockResponse ? mockResponse : {status: 404})
   })
-}
-
-// export const mockBaseClient = {
-//   BaseClient: jest.fn().mockImplementation(() => {
-//     return {
-//       grant: () => mockOIDTokenSet
-//     }
-//   })
-// }
-
-export const mockOIDTokenSet: TokenSet = {
-  access_token: "ACCESS_TOKEN",
-  expires_in: 200,
-  expired: () => false,
-  claims: () => ({
-    aud: 'test',
-    exp: 9999999999,
-    iat: 0,
-    iss: 'test',
-    sub: 'test',
-  }),
 }
